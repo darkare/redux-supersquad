9daff4beb6bb246190677670ca629846612f1981
Daryl Fong <dfong@adasasistemas.onmicrosoft.com>
2020-03-28 10:30:03 +1100

    add connector

--- src/component/CharacterList.js
+++ src/component/CharacterList.js
@@ -4,18 +4,33 @@ import {connect} from 'react-redux';
 class CharacterList extends Component {
 
     render () {
+        console.log('clist', this.props.Characters);
         return (
           <div>
               <h4>Characters</h4>
+              <ul>
+                { 
+                  this.props.Characters.map (
+                     c =>  (
+                               <li key={c.id}>
+                                   <div>
+                                        {c.name}
+                                   </div>
+                               </li>
+                            )
+                         
+                  )
+                } 
+              </ul>
           </div>
         )
     }
 }
 
-function mapStateToProps(state) {
-    console.log('state', state);
-    return {};
+function mapStateToProps1(state) {
+    //console.log('state', state);
+    return {Characters: state.characters};
 }
 
 
-export default connect(mapStateToProps, null) (CharacterList);
+export default connect(mapStateToProps1, null) (CharacterList);
